aa9840020a0cfc0449d68b177c12b4e777ba8672
fernandes-draw <fernandes_draw@hotmail.com>
2023-07-28 22:33:09 -0300

    crud estado

---
+++ backend/src/main/java/com/fundimar/backend/repositories/EstadoRepository.java
@@ -0,0 +1,6 @@
+package com.fundimar.backend.repositories;
+
+import com.fundimar.backend.entities.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long> {}
